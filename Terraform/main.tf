99bbcdb522c97825a764e1221061afb37968266d
Volodymyr Markiv <vov4uk21@gmail.com>
2021-05-30 14:25:58 +0300

    add module vpc to main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -26,6 +26,30 @@ resource "aws_instance" "Jenkins_Controller" {
 }
 
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-  cidr   = var.vpc_cidr_block
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.0.0"
+
+  name = "final-task-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+  # Skip creation of EIPs for the NAT Gateways
+  reuse_nat_ips = true
+  # IPs specified here as input to the module
+  external_nat_ip_ids = aws_eip.nat.*.id
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+resource "aws_eip" "nat" {
+  count = 3
+
+  vpc = true
 }
--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -12,12 +12,12 @@ variable "aws_ec2_instance" {
 
 }
 
-variable "vpc_cidr_block" {
-  description = "CIDR block for VPC"
-  type        = string
-  default     = "172.16.10.0/24"
-
-}
+#variable "vpc_cidr_block" {
+#  description = "CIDR block for VPC"
+#  type        = string
+#  default     = "172.16.10.0/24"
+#
+#}
 
 variable "aws_availability_zone" {
   description = "default AWS availability zone"
