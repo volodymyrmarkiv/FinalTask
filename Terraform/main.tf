e1230e6854117bb1cc65b22712c63e11d0bf88d1
Volodymyr Markiv <vov4uk21@gmail.com>
2021-06-01 13:27:27 +0300

    var changes in main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -6,7 +6,6 @@ terraform {
   }
 }
 
-
 provider "aws" {
   profile = "default"
   region  = var.aws_region
@@ -27,8 +26,11 @@ resource "aws_instance" "jenkins_controller" {
   ami               = data.aws_ami.amazon_linux.id
   instance_type     = var.aws_ec2_instance
   availability_zone = var.aws_availability_zone
-  subnet_id         = var.aws_subnet_id
-  key_name          = var.aws_private_key
+  subnet_id         = aws_subnet.subnet_ft.id
+  #subnet_id         = var.aws_subnet_id
+  key_name   = var.aws_private_key
+  private_ip = "172.16.0.10"
+  #vpc_id   = aws_vpc.vpc_ft.id
   tags = {
     Name    = "Jenkins Controller"
     OS      = "Amazon Linux"
@@ -38,8 +40,11 @@ resource "aws_instance" "jenkins_controller" {
 
 # Create Elastic IP for Jenkins Controller
 resource "aws_eip" "ip_jenkins_controller" {
-  vpc      = true
-  instance = aws_instance.jenkins_controller.id
+  vpc = true
+  #enable_dns_hostnames = true
+  instance                  = aws_instance.jenkins_controller.id
+  associate_with_private_ip = "172.16.0.10"
+  depends_on                = [aws_internet_gateway.gw]
 }
 
 ###################################################
@@ -48,8 +53,9 @@ resource "aws_instance" "web_server" {
   ami               = data.aws_ami.amazon_linux.id
   instance_type     = var.aws_ec2_instance
   availability_zone = var.aws_availability_zone
-  subnet_id         = var.aws_subnet_id
+  subnet_id         = aws_subnet.subnet_ft.id
   key_name          = var.aws_private_key
+  private_ip        = "172.16.0.11"
   tags = {
     Name    = "Web-server (Jen agt)"
     OS      = "Amazon Linux"
@@ -59,8 +65,11 @@ resource "aws_instance" "web_server" {
 
 # Create Elastic IP for Web-server
 resource "aws_eip" "ip_web_server" {
-  vpc      = true
-  instance = aws_instance.web_server.id
+  vpc = true
+  #enable_dns_hostnames = true
+  instance                  = aws_instance.web_server.id
+  associate_with_private_ip = "172.16.0.11"
+  depends_on                = [aws_internet_gateway.gw]
 }
 
 ###################################################
@@ -70,9 +79,34 @@ resource "aws_db_instance" "RDS" {
   engine              = "mariadb"
   engine_version      = "10.5.8"
   instance_class      = "db.t2.micro"
-  name                = "Rds ft"
+  name                = "rds_ft"
   username            = "admin"
   password            = "finaltask"
   skip_final_snapshot = true
   availability_zone   = var.aws_availability_zone
+}
+
+###################################################
+
+resource "aws_vpc" "vpc_ft" {
+  cidr_block = "172.16.0.0/16"
+
+  tags = {
+    Name = "Final task"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.vpc_ft.id
+}
+
+resource "aws_subnet" "subnet_ft" {
+  vpc_id                  = aws_vpc.vpc_ft.id
+  cidr_block              = "172.16.0.0/24"
+  availability_zone       = var.aws_availability_zone
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "Final task"
+  }
+  depends_on = [aws_internet_gateway.gw]
 }
