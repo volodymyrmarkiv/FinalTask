c6815f37d5ef2809909eb90bfc82ddfeaad06cc3
Volodymyr Markiv <vov4uk21@gmail.com>
2021-06-03 20:35:40 +0300

    almost working script

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -12,6 +12,16 @@ provider "aws" {
   region = "eu-central-1"
 }
 
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+/*
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.0.0"
@@ -25,17 +35,126 @@ module "vpc" {
 
   enable_nat_gateway = var.vpc_enable_nat_gateway
 
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
   tags = var.vpc_tags
+} */
+#################################################################
+
+resource "aws_vpc" "default" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
 }
 
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.default.id
+}
+
+resource "aws_subnet" "eu_central_subnet" {
+  vpc_id                  = aws_vpc.default.id
+  cidr_block              = "172.31.0.0/24"
+  map_public_ip_on_launch = true
+
+  depends_on = [aws_internet_gateway.gw]
+}
+#################################################################
+resource "aws_security_group" "ssh_sg" {
+  tags = {
+    type = "SSH security group"
+  }
+}
+
+resource "aws_security_group_rule" "ssh_rule" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.ssh_sg.id
+}
+
+resource "aws_security_group" "jenkins_sg" {
+  tags = {
+    type = "SSH security group"
+  }
+}
+
+resource "aws_security_group_rule" "jenkins_rule" {
+  type              = "egress"
+  from_port         = 8080
+  to_port           = 8080
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.jenkins_sg.id
+}
+
+resource "aws_instance" "jen_controller" {
+  ami               = data.aws_ami.amazon_linux.id
+  instance_type     = var.ec2_instance_type
+  availability_zone = "eu-central-1c"
+  key_name          = "FinalTaskEPAM"
+  private_ip        = "172.31.0.10"
+  #subnet_id         = aws_subnet.eu_central_subnet.id
+
+  # Add instance to security group
+  vpc_security_group_ids = ["sg-fea3788e",
+    aws_security_group.ssh_sg.id,
+    aws_security_group.jenkins_sg.id
+  ]
+
+  tags = {
+    Name = "Jen/Ans Controller"
+  }
+}
+
+resource "aws_instance" "web_server" {
+  ami               = data.aws_ami.amazon_linux.id
+  instance_type     = var.ec2_instance_type
+  availability_zone = "eu-central-1c"
+  key_name          = "FinalTaskEPAM"
+  private_ip        = "172.31.0.11"
+  #subnet_id         = aws_subnet.eu_central_subnet.id
+
+  # Add instance to security group
+  vpc_security_group_ids = ["sg-fea3788e",
+    aws_security_group.ssh_sg.id,
+    aws_security_group.jenkins_sg.id
+  ]
+
+  tags = {
+    Name = "Web-server"
+  }
+}
+
+resource "aws_eip" "eip_jen_controller" {
+  instance = aws_instance.jen_controller.id
+  vpc      = true
+}
+
+resource "aws_eip" "eip_web_server" {
+  instance = aws_instance.web_server.id
+  vpc      = true
+}
+# Static ip for Jenkins/Ansible Controller
+/*
+resource "aws_eip" "static_ip" {
+  vpc = true
+
+  instance = aws_instance.jen_controller.id
+  associate_with_private_ip = "172.31.0.10"
+  depends_on = [aws_internet_gateway.gw]
+}
+*/
+/*
 module "ec2_instances" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "2.19.0"
 
-  name           = "my-ec2-cluster"
+  name           = "my-cluster"
   instance_count = 2
 
-  ami                    = "ami-043097594a7df80ec"
+  ami                    = data.aws_ami.amazon_linux.id
   instance_type          = "t2.micro"
   vpc_security_group_ids = [module.vpc.default_security_group_id]
   subnet_id              = module.vpc.public_subnets[0]
@@ -45,3 +164,4 @@ module "ec2_instances" {
     Environment = "dev"
   }
 }
+*/
--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -3,31 +3,31 @@
 variable "vpc_name" {
   description = "Name of VPC"
   type        = string
-  default     = "example-vpc"
+  default     = "main-vpc"
 }
 
 variable "vpc_cidr" {
   description = "CIDR block for VPC"
   type        = string
-  default     = "10.0.0.0/16"
+  default     = "172.31.0.0/16"
 }
 
 variable "vpc_azs" {
   description = "Availability zones for VPC"
   type        = list(string)
-  default     = ["eu-central-1b"]
+  default     = ["eu-central-1c"]
 }
 
 variable "vpc_private_subnets" {
   description = "Private subnets for VPC"
   type        = list(string)
-  default     = ["10.0.1.0/24"]
+  default     = ["172.31.1.0/24"]
 }
 
 variable "vpc_public_subnets" {
   description = "Public subnets for VPC"
   type        = list(string)
-  default     = ["10.0.101.0/24"]
+  default     = ["172.31.101.0/24"]
 }
 
 variable "vpc_enable_nat_gateway" {
@@ -43,4 +43,10 @@ variable "vpc_tags" {
     Terraform   = "true"
     Environment = "dev"
   }
+}
+
+variable "ec2_instance_type" {
+  description = "Default EC2 instance"
+  type        = string
+  default     = "t2.micro"
 }
