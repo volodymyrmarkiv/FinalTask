0e0457d39186c5a16e6c964f20b61a94d9048f19
Volodymyr Markiv <vov4uk21@gmail.com>
2021-06-14 20:12:51 +0300

    upd

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -21,26 +21,6 @@ data "aws_ami" "amazon_linux" {
     values = ["amzn2-ami-hvm-*-x86_64-gp2"]
   }
 }
-/*
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "3.0.0"
-
-  name = var.vpc_name
-  cidr = var.vpc_cidr
-
-  azs             = var.vpc_azs
-  private_subnets = var.vpc_private_subnets
-  public_subnets  = var.vpc_public_subnets
-
-  enable_nat_gateway = var.vpc_enable_nat_gateway
-
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-
-  tags = var.vpc_tags
-} */
-#################################################################
 
 resource "aws_vpc" "default" {
   cidr_block           = var.vpc_cidr
