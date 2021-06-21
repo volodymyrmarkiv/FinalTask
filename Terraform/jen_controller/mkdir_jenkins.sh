6f196b6ddc135f130f5f267605aa978c48dff9c5
Volodymyr Markiv <vov4uk21@gmail.com>
2021-06-21 14:43:57 +0300

    upd mkdir_jenkins.sh and outputs.tf

--- Terraform/jen_controller/mkdir_jenkins.sh
+++ Terraform/jen_controller/mkdir_jenkins.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-dir=/home/ec2-user
 name=jenkins
 
-if [ $PWD = "$dir" ]; then
+if [[ -d ~ ]]; then
     mkdir $name
 else
     cd && mkdir $name
--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -1,19 +1,29 @@
 output "jen-ans_controller_ip" {
   description = "Public IP addresses of Jenkins/Ansible Controller EC2 instance"
   value       = aws_instance.jen_controller.public_ip
+  depends_on = [
+    aws_eip.eip_jen_controller,
+    aws_instance.jen_controller
+  ]
 }
 
 output "web-server_ip" {
   description = "Public IP addresses of Web-server EC2 instance"
   value       = aws_instance.web_server.public_ip
+  depends_on = [
+    aws_eip.eip_web_server
+  ]
 }
 
 output "build_ip" {
   description = "Public IP addresses of Jenkins Agent (build) EC2 instance"
   value       = aws_instance.build.public_ip
+  depends_on = [
+    aws_eip.eip_build
+  ]
 }
 
 output "sonarqube_ip" {
   description = "Public IP addresses of Sonarqube EC2 instance"
-  value       = aws_instance.test.public_ip
+  value       = aws_eip.test.public_ip
 }
